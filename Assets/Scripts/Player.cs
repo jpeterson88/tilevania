f400a1ac32ce87f7e266cb8abe9c661876b45384
Justin Peterson <jpeterson4223@yahoo.com>
2019-01-29 17:37:14 -0800

    added initial project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    bool isAlive = true;
+
+    //Cached component references
+    Rigidbody2D playerRigidBody2d;
+    Animator playerAnimator;
+
+    void Start()
+    {
+        playerRigidBody2d = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        Jump();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, playerRigidBody2d.velocity.y);
+        playerRigidBody2d.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidBody2d.velocity.x) > Mathf.Epsilon;
+        playerAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            playerRigidBody2d.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidBody2d.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidBody2d.velocity.x), 1f);
+        }
+    }
+}
